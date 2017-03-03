b15892bab5fccfa3f41e0fab9766288979c057c4
Bernard Lee <bernardlee82@gmail.com>
2017-03-03 14:40:41 -0500

    Add trailing commas to the test arrays
    Add commas after the last element in arrays and flattenedArrays to
    satisfy the linter.

--- spec/challenge.spec.js
+++ spec/challenge.spec.js
@@ -23,7 +23,7 @@ describe('flatten()\'s return value', () => {
     [0, [1], [[2], 3], [[[4], 5], 6]],
     [0, [1], [2, [3]], [4, [5], [6, [7]]]],
     [0, [1, []], [2, [], [3]], [4, [], [5, [], [6]], [[[[7], 8, [9]]]]]],
-    [0, [1], [true, false], [['', 'string'], [[{}], []]], [[null], [undefined]]]
+    [[0, 1], [true, false], [['', 'string'], [{}, []]], [[null], [undefined]]],
   ];
   const flattenedArrays = [
     [],
@@ -33,7 +33,7 @@ describe('flatten()\'s return value', () => {
     [0, 1, 2, 3, 4, 5, 6],
     [0, 1, 2, 3, 4, 5, 6, 7],
     [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [0, 1, true, false, '', 'string', {}, null, undefined]
+    [0, 1, true, false, '', 'string', {}, null, undefined],
   ];
 
   it('is an array', () => {

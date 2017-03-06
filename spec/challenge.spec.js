ea6aeec0902b9f1f2fcca7257e08cbaa10f5d26f
Bernard Lee <bernardlee82@gmail.com>
2017-03-06 08:08:59 -0500

    Update and add tests
    Reorganize tests into Requirements and Bonus sections.  Add the test for
    the Bonus.

--- spec/challenge.spec.js
+++ spec/challenge.spec.js
@@ -8,49 +8,127 @@ const expect = chai.expect;
 
 const Array = require('../lib/challenge');
 
-describe('flatten()', () => {
-  it('is a method defined on Array\'s prototype', () => {
-    expect(Array).to.respondTo('flatten');
+const arrays = [
+  [],
+  [[]],
+  [0, 1, 2, 3],
+  [0, [1], [[2]], [[[3]]]],
+  [0, [1], [[2], 3], [[[4], 5], 6]],
+  [0, [1], [2, [3]], [4, [5], [6, [7]]]],
+  [0, [1, []], [2, [], [3]], [4, [], [5, [], [6]], [[[[7], 8, [9]]]]]],
+  [[0, 1], [true, false], [['', 'string'], [{}, []]], [[null], [undefined]]],
+];
+
+describe('Requirements', () => {
+  describe('flatten()', () => {
+    it('is a method defined on Array\'s prototype.', () => {
+      expect(Array).to.respondTo('flatten');
+    });
+  });
+
+  describe('flatten()\'s return value', () => {
+    const flattenedArrays = [
+      [],
+      [],
+      [0, 1, 2, 3],
+      [0, 1, 2, 3],
+      [0, 1, 2, 3, 4, 5, 6],
+      [0, 1, 2, 3, 4, 5, 6, 7],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, true, false, '', 'string', {}, null, undefined],
+    ];
+
+    it('is an array.', () => {
+      arrays.forEach(array => {
+        expect(Array.isArray(array.flatten())).to.be.true;
+      });
+    });
+
+    it('does not contain any arrays.', () => {
+      arrays.forEach(array => {
+        expect(array.flatten().some(Array.isArray)).to.be.false;
+      });
+    });
+
+    it(`contains every non-array element from the original array.`, () => {
+      arrays.forEach((array, index) => {
+        expect(array.flatten()).to.deep.equal(flattenedArrays[index]);
+      });
+    });
   });
 });
 
-describe('flatten()\'s return value', () => {
-  const arrays = [
-    [],
-    [[]],
-    [0, 1, 2, 3],
-    [0, [1], [[2]], [[[3]]]],
-    [0, [1], [[2], 3], [[[4], 5], 6]],
-    [0, [1], [2, [3]], [4, [5], [6, [7]]]],
-    [0, [1, []], [2, [], [3]], [4, [], [5, [], [6]], [[[[7], 8, [9]]]]]],
-    [[0, 1], [true, false], [['', 'string'], [{}, []]], [[null], [undefined]]],
-  ];
-  const flattenedArrays = [
-    [],
-    [],
-    [0, 1, 2, 3],
-    [0, 1, 2, 3],
-    [0, 1, 2, 3, 4, 5, 6],
-    [0, 1, 2, 3, 4, 5, 6, 7],
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [0, 1, true, false, '', 'string', {}, null, undefined],
-  ];
+describe('Bonus', () => {
+  describe('flatten()', () => {
+    it('takes an argument and flattens an array to that depth.', () => {
+      const flattenedArrays = [
+        [
+          [],
+          [[]],
+          [0, 1, 2, 3],
+          [0, [1], [[2]], [[[3]]]],
+          [0, [1], [[2], 3], [[[4], 5], 6]],
+          [0, [1], [2, [3]], [4, [5], [6, [7]]]],
+          [0, [1, []], [2, [], [3]], [4, [], [5, [], [6]], [[[[7], 8, [9]]]]]],
+          [[0, 1], [true, false], [['', 'string'], [{}, []]], [[null], [undefined]]],
+        ],
+        [
+          [],
+          [],
+          [0, 1, 2, 3],
+          [0, 1, [2], [[3]]],
+          [0, 1, [2], 3, [[4], 5], 6],
+          [0, 1, 2, [3], 4, [5], [6, [7]]],
+          [0, 1, [], 2, [], [3], 4, [], [5, [], [6]], [[[[7], 8, [9]]]]],
+          [0, 1, true, false, ['', 'string'], [{}, []], [null], [undefined]],
+        ],
+        [
+          [],
+          [],
+          [0, 1, 2, 3],
+          [0, 1, 2, [3]],
+          [0, 1, 2, 3, [4], 5, 6],
+          [0, 1, 2, 3, 4, 5, 6, [7]],
+          [0, 1, 2, 3, 4, 5, [], [6], [[[7], 8, [9]]]],
+          [0, 1, true, false, '', 'string', {}, [], null, undefined],
+        ],
+        [
+          [],
+          [],
+          [0, 1, 2, 3],
+          [0, 1, 2, 3],
+          [0, 1, 2, 3, 4, 5, 6],
+          [0, 1, 2, 3, 4, 5, 6, 7],
+          [0, 1, 2, 3, 4, 5, 6, [[7], 8, [9]]],
+          [0, 1, true, false, '', 'string', {}, null, undefined],
+        ],
+        [
+          [],
+          [],
+          [0, 1, 2, 3],
+          [0, 1, 2, 3],
+          [0, 1, 2, 3, 4, 5, 6],
+          [0, 1, 2, 3, 4, 5, 6, 7],
+          [0, 1, 2, 3, 4, 5, 6, [7], 8, [9]],
+          [0, 1, true, false, '', 'string', {}, null, undefined],
+        ],
+        [
+          [],
+          [],
+          [0, 1, 2, 3],
+          [0, 1, 2, 3],
+          [0, 1, 2, 3, 4, 5, 6],
+          [0, 1, 2, 3, 4, 5, 6, 7],
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+          [0, 1, true, false, '', 'string', {}, null, undefined],
+        ],
+      ];
 
-  it('is an array', () => {
-    arrays.forEach(array => {
-      expect(Array.isArray(array.flatten())).to.be.true;
-    });
-  });
-
-  it('does not contain any arrays', () => {
-    arrays.forEach(array => {
-      expect(array.flatten().some(Array.isArray)).to.be.false;
-    });
-  });
-
-  it(`contains every non-array element from the original array`, () => {
-    arrays.forEach((array, index) => {
-      expect(array.flatten()).to.deep.equal(flattenedArrays[index]);
+      for (let d = 0; d < flattenedArrays.length; d++) {
+        arrays.forEach((array, i) => {
+          expect(array.flatten(d)).to.deep.equal(flattenedArrays[d][i]);
+        });
+      }
     });
   });
 });
